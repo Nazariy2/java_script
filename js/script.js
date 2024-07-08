7dd659ffa6eb253d4c24e325a2731ebe77d5c6a9
Nazariy2 <nazrud23@gmail.com>
2024-07-08 20:31:10 +0300

    a

---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+// var x = 3;
+// var y = 5;
+// var z = x + y;
+
+// let x = 3;
+// let y = 5;
+// console.log(y);
+// let z = x + y;
+// console.log(z);
+// y = 3;
+// console.log(3);
+
+// const x = 3;
+// x = 5;
+
+// x = 5;
+// y = 3;
+// z = x + y;
+
+// console.log(z);
+
+// let age = 16;
+// let name = 'Nazariy';
+// let isAuthorized = false;
+// let uknown = null;
+
+// console.log('number',age);
+// console.log( 'name', name);
+// console.log('isAuthorized',isAuthorized);
+// console.log(uknown);
+
+// console.log( typeof age);
+// console.log( typeof name);
+// console.log( typeof isAuthorized);
+// console.log( typeof uknown);
+
+// alert('permission denied')
+
+// window.console.log()
+
+const isUserAccepted = confirm("please accept cookies");
+
+// console.log("isUserAccepted", isUserAccepted);
+
+// let a =10;
+// a = 'hello';
+
+// console.log(a);
+// const userData = prompt('write ur data');
+// console.log('user wrote', userData);
+
+// if(isUserAccepted) {
+//     console.log('hrttnddd');
+// } else{
+//     console.log('haaaaaaaaaaa')
+// }
+// console.log('sbsrrrhrh');
+
+// const message = isUserAccepted ? console.log("KAKGKA") : console.log("else");
+
+// console.log("message");
+
+console.log('1' + 1);
