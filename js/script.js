4b9398cb75f228220b19959438abb6ee701fcd6d
Nazariy2 <nazrud23@gmail.com>
2024-07-11 19:30:19 +0300

    a

--- js/script.js
+++ js/script.js
@@ -64,39 +64,132 @@
 
 // console.log('1' + 1);
 
-let age = 16;
-console.log('I am ',age, 'years old');
+// let age = 16;
+// console.log('I am ',age, 'years old');
 
-let userName = 'Nazariy';
-console.log( 'My name is',userName);
+// let userName = 'Nazariy';
+// console.log( 'My name is',userName);
 
-let isStudent = true;
-console.log('Nazariy is a student? =>',isStudent);
+// let isStudent = true;
+// console.log('Nazariy is a student? =>',isStudent);
 
-let myString = 'its not about how hard you hit. its about how hard you can get hit and keep moving forward';
-console.log(myString)
+// let myString = 'its not about how hard you hit. its about how hard you can get hit and keep moving forward';
+// console.log(myString)
 
-let number = 20;
-console.log(number + 10);
+// let number = 20;
+// console.log(number + 10);
 
-let myNull = null;
-console.log(myNull);
+// let myNull = null;
+// console.log(myNull);
 
-let name = prompt("Введіть ваше ім'я:");
-alert('Hello ' + name + ' !');
+// let name = prompt("Введіть ваше ім'я:");
+// alert('Hello ' + name + ' !');
 
 
-let userConfirmation = confirm("Ви хочете підтвердити дію?");
-if (userConfirmation) {
-    alert("Дякую за підтвердження!");
-} else {
-    alert("Дію відмінено!");
-}
+// let userConfirmation = confirm("Ви хочете підтвердити дію?");
+// if (userConfirmation) {
+//     alert("Дякую за підтвердження!");
+// } else {
+//     alert("Дію відмінено!");
+// }
 
-alert("Увага! Ця дія може бути небезпечною.");
-let danger = confirm("Ви впевнені, що хочете продовжити?")
-if(danger){
-    alert("Дякую за підтвердження!");
-} else {
-    alert("Дію відмінено!");
-}
+// alert("Увага! Ця дія може бути небезпечною.");
+// let danger = confirm("Ви впевнені, що хочете продовжити?")
+// if(danger){
+//     alert("Дякую за підтвердження!");
+// } else {
+//     alert("Дію відмінено!");
+// }
+
+
+// Lesson 2 Number
+
+// const a = 5;
+// const b = 3;
+// console.log( 'a = b', a == b);
+// console.log( 'b no = a', a !== b);
+
+// const x = true;
+// const y = 1;
+
+// console.log('x=y', x == y);
+
+
+// const c = 15;
+// const d 
+
+// const x =6;
+// const y =3;
+// if(y % 3 === 0){
+// console.log('pari');
+// }else{
+//     console.log('dispari');
+// }
+
+// console.log(3**3);
+// const num =16;
+// console.log(Math.sqrt(10));
+
+// const str = '25';
+// console.log(str,typeof str);
+// const num = Number(str);
+// console.log(num,typeof num)
+
+// const str = '15.23px25';
+// console.log(str,typeof str);
+// const num = Number.parseInt(str);
+// console.log(num,typeof num)
+// const num2 = Number.parseFloat(str);
+// console.log(num2, typeof num2);
+// const num3 = Number(str);
+// console.log(Number.isNaN(num3));
+// console.log(Number.isNaN(num));
+
+// const x = 0.1 + 0.2;
+// console.log(x === 0.3);
+// console.log(x);
+
+// console.log( (0.1 + 0.2)*10 === (0.3)*10 );
+// console.log( 0.1 * 10 + 0.2 * 10 === 0.3 * 10 );
+
+// console.log(Math.sqrt(9));
+// console.log(Math.pow(2, 6));
+
+// const arr = [10, 4, 2, 1, 6,];
+// console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+
+// const x = 10.23;
+// const y = 32.75;
+// const z= 10.5;
+
+// console.log(Math.floor(x));
+// console.log(Math.floor(y));
+// console.log(Math.floor(z));
+
+// console.log(Math.ceil(x));
+// console.log(Math.ceil(y));
+// console.log(Math.ceil(z));
+
+// console.log(Math.round(x));
+// console.log(Math.round(y));
+// console.log(Math.round(z));
+
+// const x = 2.23464774858;
+// console.log(x);
+
+// console.log(x.toFixed(2));
+
+// const x = 2.42444443;
+// const y =2.3;
+// const z = 2;
+
+// console.log(Number(x.toFixed(2)));
+// console.log(Number(y.toFixed(2)));
+// console.log(Number(z.toFixed(2)));
+
+const cat1 =20;
+const cat =4;
+const sum = (cat1 * 2) + (cat * 2);
+const gip = Math.sqrt(sum);
+console.log(gip);
