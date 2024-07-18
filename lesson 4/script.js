a4f8bab49fe500451672fe8018bf50bdf763c956
Nazariy2 <nazrud23@gmail.com>
2024-07-18 19:42:43 +0300

    a

---
+++ lesson 4/script.js
@@ -0,0 +1,32 @@
+// const a = "";
+// const b = "MyName";
+
+// if(a){
+//     console.log( "Correct Deal");
+// }
+// if(b){
+//     console.log()
+// }
+
+const a = "fda";
+const b = null;
+const c = true;
+const d ='Hello';
+const e = 32;
+
+// console.log( Boolean(a &&  b && c && d && e > 10));
+if(a &&  !b && c && d && e > 10){
+    console.log(" correct deal");
+}
+// console.log(a ||  b || c || d || e > 10);
+
+// if(a ||  b || c || d || e > 10){
+// console.log("incorect");
+// }
+
+// a && b && console.log('Hello');
+// c && console.log('Hello 2');
+
+// const temp = a || d;
+
+// console.log(temp);
